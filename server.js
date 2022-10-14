a745f100318cde950eccc75bce22fc155f0615a7
Bapun <bapunhansdah777@gmail.com>
2022-10-14 21:00:26 +0530

    production

--- server.js
+++ server.js
@@ -54,12 +54,12 @@ const server = new ApolloServer({
 })
 
 //server.listen(PORT,console.log(`app running at Port ${PORT}`))
-// if(process.env.NODE_ENV=="production"){
+if(process.env.NODE_ENV=="production"){
    app.use(express.static('client/build'))
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
-// }
+}
 
 await server.start();
 server.applyMiddleware({
